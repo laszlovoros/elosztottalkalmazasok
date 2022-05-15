a3c20268306ba9a251d85b2c27757044d4353d05
laszlovoros <dorzsboss@yahoo.com>
2022-05-15 09:05:44 +0200

    Ez az interfész írja le, hogy miket kell csinálnia az adatmodellt kezelő osztálynak. A GUI ezeket a függvényeket fogja hívogatni a működése  közben. Ha a GUI programozása közben új igény merül esetleg fel, akkor bővíteni fogom.

---
+++ tanardiak/src/main/java/com/mycompany/tanardiak/AdatSzervizSzolgaltato.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tanardiak;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Laci
+ * Ez az interfész írja le, hogy miket kell csinálnia az adatmodellt kezelő
+ * osztálynak. A GUI ezeket a függvényeket fogja hívogatni a működése közben.
+ */
+public interface AdatSzervizSzolgaltato {
+    public String[] getEmberTipusok(); // visszaad egy kételemű tömböt az embertipusokkal (Tanár, Diák)
+    public Vector <Ember> createEmberLista(); // Létrehoz egy üres Vectort, amit a GUI a JListhez csatol. Ebben lesznek az adatok.
+    public Ember getEmberBySorszam(int sorszam); // Átadja a lista sorszam pozíción levő elemét
+    public String getRealClass(Ember objektum); // Visszaadja, hogy az Ember az Tanar vagy Diak
+    public String deleteByAzonosito(String azonosito); /* Törli a lista azon elemét, amelyiknek 
+    az azonosítóját megadtuk. Visszaad egy üzenetet a törlés sikerességéről;*/
+    public String addNewEmber(int emberTipusIndex,String nev,String azonosito,String targy,
+            String osztaly,String atlag);
+    /*Létrehoz egy uj Tanar vagy Diak objektumot a megadott adatok alapján, és berakja az emberListába
+    Ellenőrzéseket végez, ha hiba van, akkor hibaüzenetet ad vissza, és nem hozza létre az objektumot.
+    Ha már létezik a név, azonosító, akkor generál egy olyat, ami az eredeti, de mellérak egy sorszámot.
+    Azok a paraméterek, amelyek nem relevánsak, lehetnek "" értékűek, vagy null-ok is.
+    (pld. Tanár esetében az átlag nem releváns) */
+    public String modifyEmber(int emberTipusIndex,String nev,String azonosito,String targy,
+            String osztaly,String atlag);
+    /*Módosít egy meglevő Tanar vagy Diak objektumot a megadott adatok alapján
+    Ellenőrzéseket végez, ha hiba van, akkor hibaüzenetet ad vissza, és nem módosítja az objektumot.
+    Ha már létezik a név, azonosító, akkor generál egy olyat, ami az eredeti, de mellérak egy sorszámot.
+    Azok a paraméterek, amelyek nem relevánsak, lehetnek "" értékűek, vagy null-ok is.
+    (pld. Tanár esetében az átlag nem releváns) */
+    public String copyEmber(int emberListaSorszam); // megmásolja a lista megadott elemét, új módosított
+    // névvel és azonosítóval, lásd addNewEmber metódus
+    public String[] top3Diak(String osztaly); // visszaadja az adott osztály 3 legjobb diákját
+    public String saveTop3(String osztaly); // lementi a megadott osztály 3 legjobb diákját. Sikerességről
+    // üzenetet ad vissza
+    public String[] loadTop3(String fajlNev); //Megadott fajlnevet betölti, és a benne levő 3 diákot tömbben visszaadja
+    
+    
+}
+
